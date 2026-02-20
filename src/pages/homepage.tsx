6490b1eb1997ee5aa50265cc6bfdcc1773adad72
Iván Domonkos <144695310+IvnDmnks@users.noreply.github.com>
2026-02-20 11:41:45 +0100

    feat: course logos added

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,28 +1,33 @@
 import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 
 const CourseList = [
   {
     title: 'Webes alapok',
     link: '/docs/webes-alapok/intro',
+    logo: '/img/html-logo.svg',
     description:
       'A tanfolyamon ismerkedhettek a modern szoftverfejlesztési módszerekekkel és technikákkal, valamint a elsajátíthatjátok webfejlesztés alapjait, és megismerkedhettek a web működésével, a HTML, JavaScript és a CSS nyelvek világával. \n Emellett bevezetést kaphattok a git működésébe és gyakorlati használatába, lehetővé téve munkátok gyorsabb és hatékonyabb elvégzését.',
   },
   {
     title: 'Node.js',
     link: '/docs/node-js/intro',
+    logo: '/img/node-js-logo.svg',
     description:
       'NodeJS tanfolyamon megismerkedhettek a TypeScript nyelvvel, a NestJS keretrendszerrel. A tanfolyam során felépítünk egy ticket management site-ot, amelyet folytatólagosan a React tanfolyamon fogjátok gyönyörű UI-jal feldobni.',
   },
   {
     title: 'SpringBoot + Kotlin',
     link: '/docs/spring-boot/intro',
+    logo: '/img/spring-boot-logo.svg',
     description:
-      'Spring Boot és Kotlin előadásokon betekintést nyerhettek egy izgalmas nyelv, illetve egy elterjedt és modern keretrendszer alapjaiba, amellyel akár enterprise minőségű appokat is képesek lesztek fejleszteni. A tanfolyam után adott a lehetőség az SCH-Pincérbe és a CMSch franchise projektekbe való becsatlakozásra. CMSch rendszerünket a közélet rengeteg eseményén megfuttatuk már, mint példul: Gólyatábor, Gólyahét, Qpa, GólyaKörTe, Gólyabál, Simonyi Hackathon.',
+      'Spring Boot és Kotlin előadásokon betekintést nyerhettek egy izgalmas nyelv, illetve egy elterjedt és modern keretrendszer alapjaiba, amellyel akár enterprise minőségű appokat is képesek lesztek fejleszteni. A tanfolyam után adott a lehetőség az SCH-Pincérbe és a CMSch franchise projektekbe való becsatlakozásra. CMSch rendszerünket a közélet rengeteg eseményén megfuttatuk már.',
   },
   {
     title: 'React',
     link: '/docs/react/intro',
+    logo: '/img/react-logo.svg',
     description:
       'HTML, CSS és TypeScript tudásunk felhasználásával megismerkedünk az egyik legnépszerűbb frontend keretrendszerrel, a ReactJS-szel, amellyel gyorsan, átláthatóan és stabilan leszünk képesek webes alkalmazásokat fejleszteni. A ReactJS alapjainak bemutatása és gyakorlati alkalmazása mellett megismerkedünk számos hasznos könyvtárral, praktikával és tudással is.',
   },
@@ -40,17 +45,30 @@ export default function HomePage() {
 
       <main className='container' style={{ marginTop: '3rem', marginBottom: '3rem' }}>
         <div className='row'>
-          {CourseList.map((name, idx) => (
-            <div key={idx} className='col col--3 margin-bottom--lg'>
-              <div className='card shadow--md display-flex' style={{ height: '100%', flexDirection: 'column' }}>
-                <div className='card__header text--center'>
-                  <h3>{name.title}</h3>
-                </div>
+          {CourseList.map((course, idx) => (
+            <div key={idx} className='col col--6 margin-bottom--lg'>
+              <div className='card shadow--md' style={{ height: '100%', flexDirection: 'column', display: 'flex' }}>
                 <div className='card__body'>
-                  <p style={{ whiteSpace: 'pre-line' }}>{name.description}</p>
+                  <div style={{ display: 'flex', alignItems: 'flex-start', gap: '1.5rem' }}>
+                    <div style={{ flexShrink: 0, paddingTop: '0.5rem' }}>
+                      <img
+                        src={useBaseUrl(course.logo)}
+                        alt={course.title}
+                        style={{ width: '60px', height: '60px', objectFit: 'contain' }}
+                      ></img>
+                    </div>
+                    <div style={{ flexGrow: 1 }}>
+                      <h3 style={{ fontSize: '2rem' }}>{course.title}</h3>
+                      <p
+                        style={{ whiteSpace: 'pre-line', fontSize: '0.9rem', lineHeight: '1.5', textAlign: 'justify' }}
+                      >
+                        {course.description}
+                      </p>
+                    </div>
+                  </div>
                 </div>
                 <div className='card__footer' style={{ marginTop: 'auto' }}>
-                  <Link className='button button--secondary button--block' to={name.link}>
+                  <Link className='button button--secondary button--block' to={course.link}>
                     Irány a tanfolyamhoz!
                   </Link>
                 </div>
